0757f356f5d19ed01df2e67081b29fbef431dc19
marcelodolacio <marcelodolacio@gmail.com>
2021-02-05 19:37:25 -0300

    atualizacao de obj

--- pages/api/pontosTuristicos.js
+++ pages/api/pontosTuristicos.js
@@ -1,13 +1,138 @@
 function domin(request, response) {
     response.json([{
-        name: "Torre de TV",
-        latitude: -15.790380481652946,
-        longitude: -47.89284237560522
-    }, {
-        name: "Torre de TV",
-        latitude: -15.772951742341565,
-        longitude: -47.92127710388373
-    }
+        id: 1,
+        nome: "Torre de TV",
+        bairro: "Plano Piloto",
+        lat: -15.790400256211047,
+        long: -47.89278139802233,
+        image: "https://lh5.googleusercontent.com/p/AF1QipPPA1j5ARkOnEa4OAH3vfqH8qyRxioPK2DlB14G=w408-h305-k-no"
+    },
+    {
+        id: 2,
+        nome: "Torre de TV Digital",
+        bairro: "Sobradinho",
+        lat: -15.696678071019761,
+        long: -47.830334102057456,
+        image: "https://lh5.googleusercontent.com/p/AF1QipPbfudWxsw7Yd1wYcpRgDMPRBzuKFWV2W_Ot4jr=w408-h544-k-no"
+    },
+
+    {
+        id: 3,
+        nome: "Catedral Nossa Senhora Aparecida",
+        bairro: "Plano Piloto",
+        lat: -15.798145002548557,
+        long: -47.875511100881376,
+        image: "https://lh5.googleusercontent.com/p/AF1QipNDVmvkKb2QJiwxdtF_whi1iI_4DQwrb8UR_H6J=w408-h271-k-no"
+    },
+
+    {
+        id: 4,
+        nome: "Ponte Juscelino Kubitschek",
+        bairro: "Lago Sul",
+        lat: -15.822721898923112,
+        long: -47.83000010203063,
+        image: "https://lh4.googleusercontent.com/proxy/8i7qWvEYIEQxa52qSDlC1iFn__v8NPHR--xrMEYh_Dztp-3mp-98mi2Iget1J2SIQWdZVn1FVhsabsrnO544pzNcr07_XPi9b7UO2_zfosedoGQ5cHhFnp_67JBv_Np6UQILcka4j6QpfDUm3z-E844R7b2ZZSk=w408-h272-k-no"
+    },
+
+    {
+        id: 5,
+        nome: "Memoriál JK",
+        bairro: "Plano Piloto",
+        lat: -15.784082397614506,
+        long: -47.913332944358736,
+        image: "https://lh5.googleusercontent.com/p/AF1QipPV-uH8bK_WK-3ARt5FHFT8ALY8r_qtl96ijnSe=w426-h240-k-no"
+    },
+
+    {
+        id: 6,
+        nome: "Capela Rainha da Paz",
+        bairro: "Plano Piloto",
+        lat: -15.780758048875795,
+        long: -47.92433015970327,
+        image: "https://lh5.googleusercontent.com/p/AF1QipPfMHApS2UzsgJA3epVqsEGryaagXsxr0-pt84H=w408-h306-k-no"
+    },
+
+    {
+        id: 7,
+        nome: "Ermida Dom Bosco",
+        bairro: "Lago Sul",
+        lat: -15.798202353369547,
+        long: -47.80948288668691,
+        image: "https://lh5.googleusercontent.com/p/AF1QipME3NefkOGSMHOf3CK-a9ajGPHFhY-J9Uv_QIj5=w408-h306-k-no"
+    },
+
+    {
+        id: 8,
+        nome: "Parque da Cidade Sarah Kubitschek",
+        bairro: "Plano Piloto",
+        lat: -15.800136719714093,
+        long: -47.90781093086694,
+        image: "https://lh5.googleusercontent.com/p/AF1QipP-qutsONMwkzBbYjYB9eI_HQtT9GLrz0r9nWcm=w408-h306-k-no"
+    },
+
+    {
+        id: 9,
+        nome: "Congresso Nacional",
+        bairro: "Plano Piloto",
+        lat: -15.799536389580831,
+        long: -47.86414144435884,
+        image: "https://lh5.googleusercontent.com/p/AF1QipPz1u-OJncCy2e8WSFIb0bD8pi1ZNhYfR1Fk9iu=w408-h305-k-no"
+    },
+
+    {
+        id: 10,
+        nome: "Palácio do Planalto",
+        bairro: "Plano Piloto",
+        lat: -15.79893533021512,
+        long: -47.86072598668668,
+        image: "https://lh5.googleusercontent.com/p/AF1QipPwvf9kgt5ddcmsYYsOhUaS0pQpiLAsp6xWx-6c=w408-h306-k-no"
+    },
+
+    {
+        id: 11,
+        nome: "Palácio da Alvorada",
+        bairro: "Plano Piloto",
+        lat: -15.792336983346733,
+        long: -47.82288864621135,
+        image: "https://lh5.googleusercontent.com/p/AF1QipNu-Ra0soyC1RORb3reZBwEZ0Il8Z2oeQJKIgMI=w408-h408-k-no"
+    },
+
+    {
+        id: 12,
+        nome: "Universidade de Brasília",
+        bairro: "Plano Piloto",
+        lat: -15.762372560557614,
+        long: -47.8703414234577,
+        image: "https://lh5.googleusercontent.com/p/AF1QipMDoZcXXtpbTjryFjfWsKgLf3XCL_2WlzFi9kxb=w408-h408-k-no"
+    },
+
+    {
+        id: 13,
+        nome: "Museu do Catetinho",
+        bairro: "Gama",
+        lat: -15.947506529742538,
+        long: -47.9883905731933,
+        image: "https://lh5.googleusercontent.com/p/AF1QipMFs_mB-RmNL-3yihE5R6UExdRyPH-kodbli48n=w408-h306-k-no"
+    },
+
+    {
+        id: 14,
+        nome: "Estádio Mané Garrincha",
+        bairro: "Plano Piloto",
+        lat: -15.783469048392229,
+        long: -47.8992088706424,
+        image: "https://lh5.googleusercontent.com/p/AF1QipNw4jyBHLU-bjAQ_tp2Mlp_wJUvRWu9-X_AXNpw=w408-h305-k-no"
+    },
+
+    {
+        id: 15,
+        nome: "Pedra Fundamental",
+        bairro: "Planaltina",
+        lat: -15.684678380181845,
+        long: -47.68107464472949,
+        image: "https://lh5.googleusercontent.com/p/AF1QipOVV5XU12kA3UNl5DlaZRIA_6Yl7eBdBHdkp3lC=w408-h544-k-no"
+    },
+
     ]);
 }
 export default domin;
